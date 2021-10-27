b383711457b497deae314e4d059e2745a02b8086
Alexey <alexey.roon@gmail.com>
2021-10-27 13:06:45 +0600

    1313. Decompress Run-Length Encoded List

---
+++ 1313-decompress-run-length-encoded-list/src/main/java/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public int[] decompressRLElist(int[] nums) {
+
+        List<Integer> result = new ArrayList();
+
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            for (int j = 0; j < nums[i]; j++) {
+                result.add(nums[i + 1]);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
---
+++ 1313-decompress-run-length-encoded-list/src/test/java/SolutionTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void Example1Test() {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4};
+        int[] output = {2, 4, 4, 4};
+        int[] answer = solution.decompressRLElist(nums);
+        assertArrayEquals(output, answer);
+    }
+
+    @Test
+    void Example2Test() {
+        Solution solution = new Solution();
+        int[] nums = {1, 1, 2, 3};
+        int[] output = {1, 3, 3};
+        int[] answer = solution.decompressRLElist(nums);
+        assertArrayEquals(output, answer);
+    }
+}
